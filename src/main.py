e44f0ccb93410260a11ddd88a73fbb6c03141cbe
OtherwiseJLo <jonathanglopez@gmail.com>
2020-08-23 09:10:04 -0400

    Pull data from url

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+import requests
+from datetime import datetime
+from datetime import timedelta
 
+table_names = ["name.basics", "title.akas", "title.basics",
+               "title.crew", "title.episodes", "title.principals", "title.ratings"]
+url = "https://datasets.imdbws.com/{}.tsv.gz"
 
-
-
-url = "https://datasets.imdbws.com/name.basics.tsv.gz"
+data = []
+total_download_time = 0.0
+for dataset in [url.format(table) for table in table_names]:
+    print(dataset.split("/")[-1])
+    download_start_time = datetime.now()
+    data.append(requests.get(dataset))
+    time_to_download: timedelta = datetime.now() - download_start_time
+    print(
+        f"{len(data[-1].content)} downloaded in time to download: {time_to_download.total_seconds()}")
+    total_download_time += time_to_download.total_seconds()
