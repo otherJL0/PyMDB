0389f463c9a487bbc96dbc7a42b6d049fa16fcd6
OtherwiseJLo <jonathanglopez@gmail.com>
2020-12-14 08:16:42 -0500

    Updated interfaces for movies

--- src/imdb_interfaces.py
+++ src/imdb_interfaces.py
@@ -1,14 +1,58 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 
 @dataclass
-class name_basics:
-    """ Class to keep track of peoples' names """
+class Title:
+    tconst: str
+    schema: str = field(init=False, default="Title")
 
-    nconst: str  # Unique alphanumeric ID
+
+@dataclass
+class Name:
+    nconst: str
+    schema: str = field(init=False, default="Name")
+
+
+@dataclass
+class Basics(Name):
     primary_name: str
     birth_year: int
     death_year: int
     primary_profession: List[str]
     known_for_titles: List[str]
+
+
+@dataclass
+class Akas(Title):
+    ordering: int
+    title: str
+    region: str
+    language: str
+    types: List[str]
+    attributes: List[str]
+    is_original_title: bool
+
+
+@dataclass
+class Basics(Title):
+    tconst: str
+    title_type: str
+    primary_title: str
+    original_title: str
+    is_adult: bool
+    start_year: int
+    end_year: int
+    runtime_minutes: int
+    genres: List[str]
+
+
+@dataclass
+class Crew(Title):
+    tconst: str
+    directors: List[str]
+
+
+@dataclass
+class Episode(Title):
+    writers: List[str]
